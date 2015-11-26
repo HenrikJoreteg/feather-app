03bfe0d5e13c002ceb777e203b3f82d7f46dc75e
Henrik Joreteg <hjoreteg@gmail.com>
2015-11-26 00:31:54 -0800

    pre-render

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
+require('babel-register')
 var getConfig = require('hjs-webpack')
-
+var toHtml = require('vdom-to-html')
+var ui = require('./src/ui').default
 
 module.exports = getConfig({
   in: 'src/app.js',
   out: 'public',
   clearBeforeBuild: true,
   html: function (context) {
-    return context.defaultTemplate({html: '<div></div>'})
+    return {
+      'one.html': context.defaultTemplate({html: toHtml(ui({url: '/one', count: 0}))}),
+      'two.html': context.defaultTemplate({html: toHtml(ui({url: '/two', count: 0}))}),
+      'index.html': context.defaultTemplate({html: toHtml(ui({url: '/', count: 0}))})
+    }
   }
 })
